18f6dc9e1b44131ec1ca9a1f1eb4feae46b5ba70
LeakyRUS <vladislav.zhilin@gmail.com>
2022-08-01 22:24:42 +0300

    Fix tabs

--- MicrophoneSelectFix/MicroSelectFix.cs
+++ MicrophoneSelectFix/MicroSelectFix.cs
@@ -10,36 +10,36 @@ using ABI_RC.Core;
 
 namespace MicrophoneSelectFix
 {
-	public class MicroSelectFix : MelonMod
-	{
-		public override void OnApplicationStart()
-		{
-			MelonLogger.Msg("LeakyRUS's MicrophoneSelectFix mod loaded");
-		}
+    public class MicroSelectFix : MelonMod
+    {
+        public override void OnApplicationStart()
+        {
+            MelonLogger.Msg("LeakyRUS's MicrophoneSelectFix mod loaded");
+        }
 
-		[HarmonyPatch(typeof(Audio), nameof(Audio.GetAudioInputDevices))]
-		public class Fix
-		{
-			// The original method has 'void' return type. This is a hack to only call the new method.
-			// Thanks @SDraw for the tip.
-			public static bool Prefix()
-			{
-				List<UiKeyValue> list = new List<UiKeyValue>()
+        [HarmonyPatch(typeof(Audio), nameof(Audio.GetAudioInputDevices))]
+        public class Fix
+        {
+            // The original method has 'void' return type. This is a hack to only call the new method.
+            // Thanks @SDraw for the tip.
+            public static bool Prefix()
+            {
+                List<UiKeyValue> list = new List<UiKeyValue>()
                 {
-					new UiKeyValue { value = "Default" }
-				};
-				foreach (string text in Microphone.devices)
-				{
-					list.Add(new UiKeyValue
-					{
-						key = text,
-						value = CVRTools.TruncateString(text, 40)
-					});
-				}
-				ViewManager.Instance.SendMicrophoneList(list);
+                    new UiKeyValue { value = "Default" }
+                };
+                foreach (string text in Microphone.devices)
+                {
+                    list.Add(new UiKeyValue
+                    {
+                        key = text,
+                        value = CVRTools.TruncateString(text, 40)
+                    });
+                }
+                ViewManager.Instance.SendMicrophoneList(list);
 
-				return false;
-			}
-		}
-	}
+                return false;
+            }
+        }
+    }
 }
